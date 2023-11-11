9b8df1301d2bbabfe0202243879284a49efdd3b5
zhandos <yernazarovz@gmail.com>
2023-11-12 02:45:07 +0600

    redeploy

--- src/main/java/kz/demo/halykoiy/HalykOiyApplication.java
+++ src/main/java/kz/demo/halykoiy/HalykOiyApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HalykOiyApplication {
-
+    // redeploy 02:44
     public static void main(String[] args) {
         SpringApplication.run(HalykOiyApplication.class, args);
     }
