bb5b059e1b0cf0b424cb4f51f3d4491279929136
zhandos <yernazarovz@gmail.com>
2023-11-12 02:54:39 +0600

    redeploy

--- src/main/java/kz/demo/halykoiy/config/FlywayRunner.java
+++ src/main/java/kz/demo/halykoiy/config/FlywayRunner.java
@@ -20,7 +20,20 @@ public class FlywayRunner implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         jdbcTemplate.execute("DROP TABLE IF EXISTS public.flyway_schema_history");
-        flyway.repair();  // Recreates the schema history table
+        String sql = "CREATE TABLE IF NOT EXISTS \"public\".\"flyway_schema_history\" (" +
+                "    \"installed_rank\" INT NOT NULL," +
+                "    \"version\" VARCHAR(50)," +
+                "    \"description\" VARCHAR(200) NOT NULL," +
+                "    \"type\" VARCHAR(20) NOT NULL," +
+                "    \"script\" VARCHAR(1000) NOT NULL," +
+                "    \"checksum\" INT," +
+                "    \"installed_by\" VARCHAR(100) NOT NULL," +
+                "    \"installed_on\" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP," +
+                "    \"execution_time\" INT NOT NULL," +
+                "    \"success\" BOOLEAN NOT NULL," +
+                "    CONSTRAINT \"flyway_schema_history_pk\" PRIMARY KEY (\"installed_rank\")" +
+                ");";
+        jdbcTemplate.execute(sql);
         flyway.migrate(); // Runs the migrations
 
     }
