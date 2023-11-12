f5c8fc16d084d7347bfe056c71d8e21202e9fd3a
zhandos <yernazarovz@gmail.com>
2023-11-12 07:23:38 +0600

    redeploy

--- src/main/java/kz/demo/halykoiy/services/impl/JwtServiceImpl.java
+++ src/main/java/kz/demo/halykoiy/services/impl/JwtServiceImpl.java
@@ -8,8 +8,10 @@ import java.util.Map;
 import java.util.function.Function;
 
 import kz.demo.halykoiy.services.JwtService;
+import kz.demo.halykoiy.services.UserService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
 
@@ -36,7 +38,7 @@ public class JwtServiceImpl implements JwtService {
     @Override
     public boolean isTokenValid(String token, UserDetails userDetails) {
         final String userName = extractUserName(token);
-        return (userName.equals(userDetails.getUsername())) && !isTokenExpired(token);
+        return userName.equals(userDetails.getUsername());
     }
 
     private <T> T extractClaim(String token, Function<Claims, T> claimsResolvers) {
@@ -51,17 +53,9 @@ public class JwtServiceImpl implements JwtService {
                 .signWith(getSigningKey(), SignatureAlgorithm.HS256).compact();
     }
 
-    private boolean isTokenExpired(String token) {
-        return extractExpiration(token).before(new Date());
-    }
-
-    private Date extractExpiration(String token) {
-        return extractClaim(token, Claims::getExpiration);
-    }
-
     private Claims extractAllClaims(String token) {
-        return Jwts.parserBuilder().setSigningKey(getSigningKey()).build().parseClaimsJws(token)
-                .getBody();
+            return Jwts.parserBuilder().setSigningKey(getSigningKey()).build().parseClaimsJws(token)
+                    .getBody();
     }
 
     private Key getSigningKey() {
